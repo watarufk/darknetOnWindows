c0c3e611008f14104ef0655e48bd8b8cb7b4a51a
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-06-27 23:50:59 +0900

    Add binary files to Examples and update sample batch files

--- Examples/TinyYoloWithCamera.bat
+++ Examples/TinyYoloWithCamera.bat
@@ -1,4 +1,4 @@
 REM https://pjreddie.com/darknet/yolo/
 
 cd ..\darknet-win32
-darknet detector demo "cfg\coco.data" "cfg\tiny-yolo.cfg" "D:\Data\darknet\tiny-yolo.weights"
+"..\Examples\darknet.exe" detector demo "cfg\coco.data" "cfg\tiny-yolo.cfg" "D:\Data\darknet\tiny-yolo.weights"
--- Examples/YoloWithCamera.bat
+++ Examples/YoloWithCamera.bat
@@ -1,4 +1,4 @@
 REM https://pjreddie.com/darknet/yolo/
 
 cd ..\darknet-win32
-darknet detector demo "cfg\coco.data" "cfg\yolo.cfg" "D:\Data\darknet\yolo.weights"
+"..\Examples\darknet.exe" detector demo "cfg\coco.data" "cfg\yolo.cfg" "D:\Data\darknet\yolo.weights"
