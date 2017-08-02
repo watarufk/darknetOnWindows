5e3a3797940b457f5e8dcb9efbaefca148e684cc
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-08-02 11:42:16 +0900

    Update darknet and merge the changes

--- Projects/darknet-win32/src/gemm.h
+++ Projects/darknet-win32/src/gemm.h
@@ -1,34 +1,28 @@
 #ifndef GEMM_H
 #define GEMM_H
 
-// exception occurs
-//#define INTEL_MKL
 
+//#define INTEL_MKL // exception occurs
 #ifdef INTEL_MKL
-
 #include "C:/Program Files (x86)/IntelSWTools/compilers_and_libraries_2017.2.187/windows/mkl/include/mkl_blas.h"
-
 #else
+void gemm_bin(int M, int N, int K, float ALPHA, 
+        char  *A, int lda, 
+        float *B, int ldb,
+        float *C, int ldc);
+        
+void gemm(int TA, int TB, int M, int N, int K, float ALPHA, 
+                    float *A, int lda, 
+                    float *B, int ldb,
+                    float BETA,
+                    float *C, int ldc);
 
-void gemm_bin(int M, int N, int K, float ALPHA,
-    char  *A, int lda,
-    float *B, int ldb,
-    float *C, int ldc);
-
-void gemm(int TA, int TB, int M, int N, int K, float ALPHA,
-    float *A, int lda,
-    float *B, int ldb,
-    float BETA,
-    float *C, int ldc);
-
-void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
-    float *A, int lda,
-    float *B, int ldb,
-    float BETA,
-    float *C, int ldc);
-
+void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA, 
+        float *A, int lda, 
+        float *B, int ldb,
+        float BETA,
+        float *C, int ldc);
 #endif
-
 #ifdef GPU
 void gemm_gpu(int TA, int TB, int M, int N, int K, float ALPHA, 
         float *A_gpu, int lda, 
